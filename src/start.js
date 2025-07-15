6ec6f1bd8f91f5d453ebfe6b311a7784523d3d09
Stephan Zlatarev <stephan.zlatarev@gmail.com>
2025-07-15 09:04:45 +0300

    Remove extract.js

--- src/start.js
+++ src/start.js
@@ -1,3 +1,22 @@
-import extract from "./extract.js";
+import { MpqFile } from "@robobays/mpq-reader";
+import Replay from "./replay/replay.js";
+import output from "./output/svg.js";
+import getTimeline from "./timeline/timeline.js";
 
-extract();
+async function start() {
+  try {
+    const mpq = await MpqFile.load();
+    const replay = new Replay(mpq);
+
+    for (const warning of replay.warnings) {
+      console.error(warning);
+    }
+
+    await output(process.stdout, getTimeline(replay), { map: replay.mapFileName });
+  } catch (error) {
+    console.error(error.message);
+    console.log(`<svg xmlns="http://www.w3.org/2000/svg"><text x="10" y="12">${error.message}</text></svg>`);
+  }
+}
+
+start();
